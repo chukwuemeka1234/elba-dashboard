328f770fd8c1d547db50f7ad504c96b16e4e50f8
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-08-16 10:54:52 +0100

    Syncfusion

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import './index.css';
+import { registerLicense } from '@syncfusion/ej2-base';
 import App from './App';
+
+//Registration Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2V1hhQlJAfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5bd0NjXH9edXdWT2hc')
 
 ReactDOM.render(<App />, document.getElementById('root'));
