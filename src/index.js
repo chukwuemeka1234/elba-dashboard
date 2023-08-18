d189061ef779a9f8929bd79624dad601ae553f20
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-08-18 22:48:56 +0100

    usestatecontext for sidebar closing

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import ReactDOM from "react-dom";
 import './index.css';
 import { registerLicense } from '@syncfusion/ej2-base';
 import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
 //Registration Syncfusion license key
 registerLicense('ORg4AjUWIQA/Gnt2V1hhQlJAfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5bd0NjXH9edXdWT2hc')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById('root'));
